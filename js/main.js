45906824d2c34d2208bbde77448483a58fc3a78d
Vahe Safaryan <vsafaryan50@gmail.com>
2023-04-10 13:22:31 +0400

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 "use strict";
 
 // HEADER TIMER START //
-
-let countDownDate = new Date("April21, 2023 20:20:00").getTime();
+let countDownDate = new Date("April29, 2023 20:20:00").getTime();
 const elements = Array.from(document.getElementsByClassName("timers"));
 
 function timer() {
@@ -53,27 +52,162 @@ for (let i = 0; i < boxes.length; i++) {
 //  PACIFIC TIME END //
 
 
-// FAQ SHOW ALL START //
-let skillsBtns = document.getElementsByClassName("expend-button");
-let skillsSection = document.querySelector(".skills-section");
-for(let skillsBtn of skillsBtns) {
-    skillsBtn.addEventListener("click", function () {
-        skillsSection.classList.toggle('expended')
-    });
-}
-// FAQ SHOW ALL END //
-
-
-
 //  ACCARDION START //
 const faqAccordion = document.querySelectorAll('.faq-section-accordion-box')
 
 for (let i = 0; i < faqAccordion.length; i++) {
     faqAccordion[i].addEventListener("click", function (event) {
-        event.currentTarget.classList.toggle('ac tive')
+        event.currentTarget.classList.toggle('active')
     })
 }
 //  ACCARDION END //
+
+
+//  POPUP START //
+const openModal = document.querySelectorAll('.openModal');
+const popupSection = document.querySelector('.popup-section');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close')
+const popupJoin = document.querySelector('.popup-join')
+const popupForm = document.querySelector('.popup-form')
+const popupDone = document.querySelector('.popup-done')
+const doneBtn = document.querySelector('.done-btn')
+
+for (let i = 0; i < openModal.length; i++) {
+    openModal[i].addEventListener('click', function (event) {
+        popupSection.classList.toggle('d-block');
+        overlay.classList.toggle('d-block');
+        event.preventDefault();
+    });
+}
+overlay.addEventListener('click', function () {
+    popupSection.classList.toggle('d-block');
+    overlay.classList.toggle('d-block');
+});
+
+
+if (closeButton) {
+    closeButton.addEventListener('click', function (e) {
+        e.preventDefault()
+        popupSection.classList.toggle('d-block');
+        overlay.classList.toggle('d-block');
+    })
+}
+
+if (popupJoin) {
+    popupJoin.addEventListener('click', function (e) {
+        e.preventDefault()
+        popupForm.classList.toggle('d-none');
+        popupDone.classList.toggle('d-none');
+    })
+}
+
+if (doneBtn) {
+    doneBtn.addEventListener('click', function (e) {
+        e.preventDefault()
+        popupSection.classList.toggle('d-block');
+        overlay.classList.toggle('d-block');
+    });
+}
+
+//  POPUP END //
+
+
+
+//  Menu in scroll START//
+window.addEventListener('scroll', function() {
+    let navLinksScroll = document.querySelector('.nav-links-scroll');
+    if (navLinksScroll) {
+        if (window.scrollY >= 200) {
+            navLinksScroll.style.display = 'flex';
+            navLinksScroll.style.position = 'fixed';
+            navLinksScroll.style.top = '0';
+            navLinksScroll.style.left = '50%';
+            navLinksScroll.style.transform = 'translate(-50%, -10%)';
+            navLinksScroll.style.zIndex = '3';
+            navLinksScroll.style.background = 'rgba(255, 255, 255, 0.8)';
+            navLinksScroll.style.backdropFilter = 'blur(13.5914px)';
+        } else {
+            navLinksScroll.style.display = '';
+            navLinksScroll.style.position = '';
+            navLinksScroll.style.top = '';
+            navLinksScroll.style.left = '';
+            navLinksScroll.style.transform = '';
+            navLinksScroll.style.zIndex = '';
+            navLinksScroll.style.background = '';
+            navLinksScroll.style.backdropFilter = '';
+        }
+    }
+});
+// Menu in scroll START //
+
+
+//  Header other menu scrol START //
+window.addEventListener('scroll', function() {
+    let headerSection = document.querySelector('.header-section');
+    if (headerSection) {
+        if (window.scrollY >= 200) {
+            headerSection.style.display = 'block';
+        } else {
+            headerSection.style.display = '';
+        }
+    }
+});
+//  Header other menu scroll END //
+
+
+//  Header section ships START //
+const headerSectionBtn = document.querySelector('.header-section-btn');
+
+if (headerSectionBtn) {
+    headerSectionBtn.addEventListener("click", function (e) {
+        e.preventDefault()
+        const headerShips = document.querySelector('.header-section-ships')
+        if (headerShips){
+            if (headerShips.style.display !== 'flex'){
+                headerShips.style.display = 'flex'
+            } else {
+                headerShips.style.display = 'none'
+            }
+        }
+    })
+}
+//  Header section ships END //
+
+
+//  Menu in right START //
+const mediaRightMenu = document.querySelector('.media-right-menu')
+const primaryIcon = document.querySelectorAll('.primary-icon')
+const menuMobile  = document.querySelectorAll('.menu-mobile ')
+const headerSectionClosed = document.querySelector('.header-section-closed')
+const blur = document.querySelector('.blur')
+
+for (let i = 0; i < primaryIcon.length; i++) {
+    primaryIcon[i].addEventListener('click', function () {
+        mediaRightMenu.style.right = "0px";
+        blur.classList.toggle('d-block');
+    });
+}
+
+for (let i = 0; i < menuMobile.length; i++) {
+    menuMobile[i].addEventListener('click', function () {
+        mediaRightMenu.style.right = "0px";
+        blur.classList.toggle('d-block');
+    });
+}
+
+headerSectionClosed.addEventListener('click', function () {
+    if (headerSectionClosed) {
+        mediaRightMenu.style.right = "-400px";
+        blur.classList.toggle('d-block');
+    }
+})
+
+blur.addEventListener('click', function () {
+    blur.classList.toggle('d-block');
+    mediaRightMenu.style.right = "-400px";
+});
+//  Menu in right END //
 
 
 
@@ -88,58 +222,41 @@ $('.counter-section').slick({
     prevArrow: false,
     responsive: [
         {
-            breakpoint: 1024,
+            breakpoint: 1025,
             settings: {
                 slidesToShow: 4,
-                slidesToScroll: 4,
+                slidesToScroll: 2,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
             breakpoint: 998,
             settings: {
                 slidesToShow: 3,
-                slidesToScroll: 3
+                slidesToScroll: 2,
+                infinite: true,
+                dots: false
             }
         },
         {
             breakpoint: 768,
             settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1,
+                dots: false
             }
         },
         {
-            breakpoint: 500,
+            breakpoint: 414,
             settings: {
                 slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToScroll: 1,
+                dots: true
             }
         }
     ]
 });
-
-
-//  Counter start //
-// let num = document.querySelectorAll(".counterup")
-// let newHeading = Array.from(num)
-// newHeading.map((item) => {
-//     let count = 0
-//
-//     function counterup() {
-//         count++
-//         item.innerHTML = count
-//         if (count == item.dataset.number) {
-//             clearInterval(stop)
-//         }
-//     }
-//
-//     let stop = setInterval(function () {
-//         counterup()
-//     }, 1);
-// })
-// COUNTER SECTION START //
 
 
 //  BRAND SECTION START //
@@ -283,16 +400,15 @@ $('.teachers-box-section').slick({
 //  TEACHERS SECTION END //
 
 
-//  TELL. NUMBERS START (window.)
-const input = document.querySelector("#phone");
-intlTelInput(input, {
-    initialCountry: "ru",
-    preferredCountries: ["ru", "us", "gb", "de"],
-    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@16.0.2/build/js/utils.js"
-});
-//  TELL. NUMBERS END
-
-
+let inputs = document.getElementsByClassName("phone");
+for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    window.intlTelInput(input, {
+        initialCountry: "ru",
+        preferredCountries: ["ru", "us", "gb", "de"],
+        utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@16.0.2/build/js/utils.js"
+    });
+}
 
 
 
